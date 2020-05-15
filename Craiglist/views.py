3eef8e1625d5f020a60067b2f89919fbddacda93
isharrma <54413011+isharrma@users.noreply.github.com>
2020-05-15 16:48:20 +0530

    New search
    Added elements to post new search

--- Craiglist/views.py
+++ Craiglist/views.py
@@ -6,4 +6,8 @@ def home(request):
     return render(request,"base.html")
 
 def new_search(request):
-    return render(request,"Craiglist/new_search.html")
+    search = request.POST.get("search")
+    context = {
+        'search' : search ,
+    }
+    return render(request,"Craiglist/new_search.html" ,context)
