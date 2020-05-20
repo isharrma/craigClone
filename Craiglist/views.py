14668e42eb44f460b75b4060c5e3fb1b9c9b3d66
isharrma <54413011+isharrma@users.noreply.github.com>
2020-05-20 22:29:30 +0530

    Basic BeautifulSoup
    started Scrapping off craiglist

--- Craiglist/views.py
+++ Craiglist/views.py
@@ -1,14 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import requests
+from requests.compat import quote_plus
 from bs4 import BeautifulSoup
 
-
+BASE_CRAIGLIST_URL = 'https://delhi.craigslist.org/search/bbb?query={}'
 
 def home(request):
     return render(request,"base.html")
 
 def new_search(request):
     search = request.POST.get("search")
+    final_url = BASE_CRAIGLIST_URL.format(quote_plus(search))
+    response = requests.get(final_url)
+    data = response.text
+    soup =BeautifulSoup(data, features='html.parser')
+    post_titles = soup.find_all('a', {'class':'result-title'})
+    print(post_titles)
     context = {
         'search' : search ,
     }
